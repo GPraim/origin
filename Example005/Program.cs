89812ea003ceb3b07fc9b0b09ff4e0e20bcf08b5
Aleksandr Maramzin <maramzinaleksandr@gmail.com>
2023-10-10 23:14:55 +0500

    Example005 Maria

---
+++ Example005/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Представьтесь пожалуйста!");
+string nameuser = Console.ReadLine();
+if(nameuser == "Маша"){
+    Console.WriteLine("Привет, самый прекрасный человек на свете!");
+} else {
+    Console.WriteLine("Здравствуйте, " + nameuser);
+}
