2ed1631176309c2646836ae817544e4dccd6d9ed
Aleksandr Maramzin <maramzinaleksandr@gmail.com>
2023-10-10 23:23:41 +0500

    Example 006

---
+++ Example006/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 2;
+int b = 3;
+int e = 20;
+int c = 1;
+int d = 8;
+
+int max = 2;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+
+Console.WriteLine("Max = " + max);
